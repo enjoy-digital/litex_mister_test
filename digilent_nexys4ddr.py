767011cfbf86c8649e26947168afe4cf641b0f3e
Florent Kermarrec <florent@enjoy-digital.fr>
2023-04-13 22:56:22 +0200

    targets: Switch to new MiSTeR top level.

--- digilent_nexys4ddr.py
+++ digilent_nexys4ddr.py
@@ -29,6 +29,8 @@ from litedram.phy import s7ddrphy
 
 from liteeth.phy.rmii import LiteEthPHYRMII
 
+from gateware.mister import MiSTeR
+
 # CRG ----------------------------------------------------------------------------------------------
 
 class _CRG(LiteXModule):
@@ -99,121 +101,17 @@ class BaseSoC(SoCCore):
                 sys_clk_freq = sys_clk_freq)
 
         # MiSTeR -----------------------------------------------------------------------------------
-        control  = Signal(128)
+        self.mister = mister = MiSTeR(platform, core="template")
+        self.mister.add_control_status_csr()
+
+        # Video ------------------------------------------------------------------------------------
         vga_pads = platform.request("vga")
-        self.specials += Instance("emu",
-            # Clk/Rst.
-            i_CLK_50M          = ClockSignal("sys"),
-            i_RESET            = ResetSignal("sys"),
-
-            # HPS Bus.
-            io_HPS_BUS         = Open(), # FIXME.
-
-            # Video (Generic).
-            o_CLK_VIDEO        = Open(),
-            o_CE_PIXEL         = Open(), # FIXME.
-            o_VIDEO_ARX        = Open(), # FIXME.
-            o_VIDEO_ARY        = Open(), # FIXME.
-
-            # Video VGA.
-            o_VGA_R            = Cat(Open(), [s for s in reversed(vga_pads.r)]),
-            o_VGA_G            = Cat(        [s for s in reversed(vga_pads.g)]),
-            o_VGA_B            = Cat(Open(), [s for s in reversed(vga_pads.b)]),
-            o_VGA_HS           = vga_pads.hsync_n,
-            o_VGA_VS           = vga_pads.vsync_n,
-            o_VGA_DE           = Open(), # FIXME.
-            o_VGA_F1           = Open(), # FIXME.
-            o_VGA_SL           = Open(), # FIXME.
-            o_VGA_SCALER       = Open(), # FIXME.
-            o_VGA_DISABLE      = Open(), # FIXME.
-
-            # HDMI.
-            i_HDMI_WIDTH       = 0,      # FIXME.
-            i_HDMI_HEIGHT      = 0,      # FIXME.
-            o_HDMI_FREEZE      = Open(), # FIXME.
-
-            # Leds.
-            o_LED_USER         = Open(), # FIXME.
-            o_LED_POWER        = Open(), # FIXME.
-            o_LED_DISK         = Open(), # FIXME.
-
-            # Buttons.
-            o_BUTTONS          = Open(), # FIXME.
-
-            # Audio.
-            i_CLK_AUDIO        = 0,      # FIXME.
-            o_AUDIO_L          = Open(), # FIXME.
-            o_AUDIO_R          = Open(), # FIXME.
-            o_AUDIO_S          = Open(), # FIXME.
-            o_AUDIO_MIX        = Open(), # FIXME.
-            io_ADC_BUS         = Open(), # FIXME.
-
-            # SDCard.
-            o_SD_SCK           = Open(), # FIXME.
-            o_SD_MOSI          = Open(), # FIXME.
-            i_SD_MISO          = 0,      # FIXME.
-            o_SD_CS            = Open(), # FIXME.
-            i_SD_CD            = 0,      # FIXME.
-
-            # DDRAM (DDR3).
-            o_DDRAM_CLK        = Open(), # FIXME.
-            i_DDRAM_BUSY       = 0,      # FIXME.
-            o_DDRAM_BURSTCNT   = Open(), # FIXME.
-            o_DDRAM_ADDR       = Open(), # FIXME.
-            i_DDRAM_DOUT       = 0,      # FIXME.
-            i_DDRAM_DOUT_READY = 0,      # FIXME.
-            o_DDRAM_RD         = Open(), # FIXME.
-            o_DDRAM_DIN        = Open(), # FIXME.
-            o_DDRAM_BE         = Open(), # FIXME.
-            o_DDRAM_WE         = Open(), # FIXME.
-
-            # SDRAM.
-            o_SDRAM_CLK        = Open(), # FIXME.
-            o_SDRAM_CKE        = Open(), # FIXME.
-            o_SDRAM_A          = Open(), # FIXME.
-            o_SDRAM_BA         = Open(), # FIXME.
-            io_SDRAM_DQ        = Open(), # FIXME.
-            o_SDRAM_DQML       = Open(), # FIXME.
-            o_SDRAM_DQMH       = Open(), # FIXME.
-            o_SDRAM_nCS        = Open(), # FIXME.
-            o_SDRAM_nCAS       = Open(), # FIXME.
-            o_SDRAM_nRAS       = Open(), # FIXME.
-            o_SDRAM_nWE        = Open(), # FIXME.
-
-            # UART.
-            i_UART_CTS         = 0,      # FIXME.
-            o_UART_RTS         = Open(), # FIXME.
-            i_UART_RXD         = 0,      # FIXME.
-            o_UART_TXD         = Open(), # FIXME.
-            o_UART_DTR         = Open(), # FIXME.
-            i_UART_DSR         = 0,      # FIXME.
-
-            # USER.
-            i_USER_IN          = 0,      # FIXME.
-            o_USER_OUT         = Open(), # FIXME.
-
-            # OSD.
-            i_OSD_STATUS       = 0,      # FIXME.
-
-            # STATUS
-            i_status           = control, # FIXME: Name.
-        )
-        platform.add_verilog_include_path("Template_MiSTer")
-        platform.add_source_dir("Template_MiSTer")
-        platform.add_source_dir("Template_MiSTer/rtl/")
-        platform.add_source_dir("Template_MiSTer/sys/")
-
-        # Control CSRs.
-        self.ctrl_rst = CSRStorage(1)
-        self.ctrl_pal = CSRStorage(1)
-        self.ctrl_col = CSRStorage(2)
-        self.ctrl_ar  = CSRStorage(2)
-
         self.comb += [
-            control[        0].eq(self.ctrl_rst.storage),
-            control[        2].eq(self.ctrl_pal.storage),
-            control[    3:3+2].eq(self.ctrl_col.storage),
-            control[121:121+2].eq( self.ctrl_ar.storage),
+            vga_pads.hsync_n.eq(mister.vga.hsync_n),
+            vga_pads.vsync_n.eq(mister.vga.vsync_n),
+            vga_pads.r.eq(mister.vga.r),
+            vga_pads.g.eq(mister.vga.g),
+            vga_pads.b.eq(mister.vga.b),
         ]
 
 # Build --------------------------------------------------------------------------------------------
--- digilent_nexys_video.py
+++ digilent_nexys_video.py
@@ -35,6 +35,8 @@ from liteeth.phy.s7rgmii import LiteEthPHYRGMII
 
 from litex.soc.cores.dma import WishboneDMAWriter
 
+from gateware.mister import MiSTeR
+
 # CRG ----------------------------------------------------------------------------------------------
 
 class _CRG(LiteXModule):
@@ -156,136 +158,13 @@ class BaseSoC(SoCCore):
             self.add_sata(phy=self.sata_phy, mode="read+write")
 
         # MiSTeR -----------------------------------------------------------------------------------
-        control    = Signal(128)
-        vga_pads = Record([
-            ("clk",     1),
-            # Synchronization signals.
-            ("hsync_n", 1),
-            ("vsync_n", 1),
-            ("de",      1),
-            # Data signals.
-            ("r",       8),
-            ("g",       8),
-            ("b",       8),
-        ])
-        self.specials += Instance("emu",
-            # Clk/Rst.
-            i_CLK_50M          = ClockSignal("emu"),
-            i_RESET            = ResetSignal("emu"),
+        self.mister = mister = MiSTeR(platform, core="template")
+        self.mister.add_control_status_csr()
 
-            # HPS Bus.
-            io_HPS_BUS         = Open(), # FIXME.
-
-            # Video (Generic).
-            o_CLK_VIDEO        = vga_pads.clk,
-            o_CE_PIXEL         = Open(), # FIXME.
-            o_VIDEO_ARX        = Open(), # FIXME.
-            o_VIDEO_ARY        = Open(), # FIXME.
-
-            # Video VGA.
-            o_VGA_R            = vga_pads.r[::-1],
-            o_VGA_G            = vga_pads.g[::-1],
-            o_VGA_B            = vga_pads.b[::-1],
-            o_VGA_HS           = vga_pads.hsync_n,
-            o_VGA_VS           = vga_pads.vsync_n,
-            o_VGA_DE           = vga_pads.de,
-            o_VGA_F1           = Open(), # FIXME.
-            o_VGA_SL           = Open(), # FIXME.
-            o_VGA_SCALER       = Open(), # FIXME.
-            o_VGA_DISABLE      = Open(), # FIXME.
-
-            # HDMI.
-            i_HDMI_WIDTH       = 0,      # FIXME.
-            i_HDMI_HEIGHT      = 0,      # FIXME.
-            o_HDMI_FREEZE      = Open(), # FIXME.
-
-            # Leds.
-            o_LED_USER         = Open(), # FIXME.
-            o_LED_POWER        = Open(), # FIXME.
-            o_LED_DISK         = Open(), # FIXME.
-
-            # Buttons.
-            o_BUTTONS          = Open(), # FIXME.
-
-            # Audio.
-            i_CLK_AUDIO        = 0,      # FIXME.
-            o_AUDIO_L          = Open(), # FIXME.
-            o_AUDIO_R          = Open(), # FIXME.
-            o_AUDIO_S          = Open(), # FIXME.
-            o_AUDIO_MIX        = Open(), # FIXME.
-            io_ADC_BUS         = Open(), # FIXME.
-
-            # SDCard.
-            o_SD_SCK           = Open(), # FIXME.
-            o_SD_MOSI          = Open(), # FIXME.
-            i_SD_MISO          = 0,      # FIXME.
-            o_SD_CS            = Open(), # FIXME.
-            i_SD_CD            = 0,      # FIXME.
-
-            # DDRAM (DDR3).
-            o_DDRAM_CLK        = Open(), # FIXME.
-            i_DDRAM_BUSY       = 0,      # FIXME.
-            o_DDRAM_BURSTCNT   = Open(), # FIXME.
-            o_DDRAM_ADDR       = Open(), # FIXME.
-            i_DDRAM_DOUT       = 0,      # FIXME.
-            i_DDRAM_DOUT_READY = 0,      # FIXME.
-            o_DDRAM_RD         = Open(), # FIXME.
-            o_DDRAM_DIN        = Open(), # FIXME.
-            o_DDRAM_BE         = Open(), # FIXME.
-            o_DDRAM_WE         = Open(), # FIXME.
-
-            # SDRAM.
-            o_SDRAM_CLK        = Open(), # FIXME.
-            o_SDRAM_CKE        = Open(), # FIXME.
-            o_SDRAM_A          = Open(), # FIXME.
-            o_SDRAM_BA         = Open(), # FIXME.
-            io_SDRAM_DQ        = Open(), # FIXME.
-            o_SDRAM_DQML       = Open(), # FIXME.
-            o_SDRAM_DQMH       = Open(), # FIXME.
-            o_SDRAM_nCS        = Open(), # FIXME.
-            o_SDRAM_nCAS       = Open(), # FIXME.
-            o_SDRAM_nRAS       = Open(), # FIXME.
-            o_SDRAM_nWE        = Open(), # FIXME.
-
-            # UART.
-            i_UART_CTS         = 0,      # FIXME.
-            o_UART_RTS         = Open(), # FIXME.
-            i_UART_RXD         = 0,      # FIXME.
-            o_UART_TXD         = Open(), # FIXME.
-            o_UART_DTR         = Open(), # FIXME.
-            i_UART_DSR         = 0,      # FIXME.
-
-            # USER.
-            i_USER_IN          = 0,      # FIXME.
-            o_USER_OUT         = Open(), # FIXME.
-
-            # OSD.
-            i_OSD_STATUS       = 0,      # FIXME.
-
-            # STATUS
-            i_status           = control, # FIXME: Name.
-        )
-        platform.add_verilog_include_path("Template_MiSTer")
-        platform.add_source_dir("Template_MiSTer")
-        platform.add_source_dir("Template_MiSTer/rtl/")
-        platform.add_source_dir("Template_MiSTer/sys/")
-
-        # Control CSRs.
-        self.ctrl_rst = CSRStorage(1)
-        self.ctrl_pal = CSRStorage(1)
-        self.ctrl_col = CSRStorage(2)
-        self.ctrl_ar  = CSRStorage(2)
-
-        self.comb += [
-            control[        0].eq(self.ctrl_rst.storage),
-            control[        2].eq(self.ctrl_pal.storage),
-            control[    3:3+2].eq(self.ctrl_col.storage),
-            control[121:121+2].eq( self.ctrl_ar.storage),
-        ]
-
+        # Video ------------------------------------------------------------------------------------
         # Video Clk Domain.
         self.cd_video = ClockDomain()
-        self.comb += self.cd_video.clk.eq(vga_pads.clk)
+        self.comb += self.cd_video.clk.eq(mister.vga.clk)
 
         # Video Capture/DMA.
 
@@ -363,13 +242,13 @@ class BaseSoC(SoCCore):
                 self.comb += self.cdc.source.connect(self.dma.sink)
 
 
-        self.video_capture = VideoCapture(vga_pads=vga_pads, fifo_depth=128)
+        self.video_capture = VideoCapture(vga_pads=mister.vga, fifo_depth=128)
         #self.comb += self.video_capture.bus.ack.eq(1)
         self.bus.add_master(name="video_capture", master=self.video_capture.bus)
 
 #        from litescope import LiteScopeAnalyzer
 #        analyzer_signals = [
-#            vga_pads,
+#            mister.vga,
 #            self.video_capture.vga_capture.fsm,
 #            self.video_capture.vga_capture.line,
 #            self.video_capture.vga_capture.pixel,
